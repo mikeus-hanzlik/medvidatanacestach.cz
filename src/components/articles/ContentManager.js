945ae343b1ff938f2d35da36d501e5ca91280e35
Michal Hanzlik <mikeus.hanzlik@gmail.com>
2018-09-02 13:50:07 +0200

    Simplified content manager

--- src/components/articles/ContentManager.js
+++ src/components/articles/ContentManager.js
@@ -1,30 +1,13 @@
 import React from "react";
-import NepalIndex from "../content/nepal/Index"
-import NepalPrehled from "../content/nepal/Prehled"
-import NepalPrijezd from "../content/nepal/Prijezd"
-import SlovinskoIndex from "../content/slovinsko/Index"
-import SlovinskoPrehled from "../content/slovinsko/Prehled"
-import SlovinskoPrijezd from "../content/slovinsko/Prijezd"
+import asyncComponent from "../infrastructure/asyncComponent";
 
 export default ({articleId, match}) => {
+    const capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
+
     const overviewPageName = "index";
     const detailId = match.params.detailId || overviewPageName;
-    let componentName = `${articleId}_${detailId}`;
 
-    const components = {
-        nepal_index: NepalIndex,
-        nepal_prehled: NepalPrehled,
-        nepal_prijezd: NepalPrijezd,
-        slovinsko_index: SlovinskoIndex,
-        slovinsko_prehled: SlovinskoPrehled,
-        slovinsko_prijezd: SlovinskoPrijezd,
-    };
+    const AsyncArticle = asyncComponent(() => import(`../content/${articleId}/${capitalizeFirstLetter(detailId)}`));
 
-    if (!(componentName in components)) {
-        componentName = `${articleId}_${overviewPageName}`
-    }
-
-    const Article = components[componentName];
-
-    return <Article />
+    return <AsyncArticle />
 }
